36d985cb2c9a148efa6326ba0b89db5d92387d3c
Aleksandr Neshcheredov <aleksandr.neshcheredov@edu.karelia.fi>
2025-04-19 17:09:09 +0300

    added procedure addDataRows

--- api/data.js
+++ api/data.js
@@ -1,26 +1,28 @@
-import { Router } from 'express';
-import {getAllData, getDataById, addData} from '../database.js'
-let router = Router()
+import { Router } from "express";
+import { getAllData, getDataById, addData, addDataRows } from "../database.js";
+let router = Router();
 
-router.get('/', async (req, res) => {
-    res.json( await getAllData() )
-})
+router.get("/", async (req, res) => {
+  res.json(await getAllData());
+});
 
-router.get('/:id', async (req, res) => {
-    res.json( await getDataById(req.params.id) )
-})
+router.get("/:id", async (req, res) => {
+  res.json(await getDataById(req.params.id));
+});
 
-router.post('/', async (req, res) => {
-    let exist = await getDataById(req.body.id)
-    if( exist[0] ) {
-        res.status(409).json( {"error": "record already exists"});
-    } else {
-        let result = await addData(req.body);
-        if(result.affectedRows)
-            res.json(req.body);
-        else
-            res.status(500).json({"error": "unknown database error"})
-    }
-})
+router.post("/", async (req, res) => {
+  let exist = await getDataById(req.body.id);
+  if (exist[0]) {
+    res.status(409).json({ error: "record already exists" });
+  } else {
+    let result = await addData(req.body);
+    if (result.affectedRows) res.json(req.body);
+    else res.status(500).json({ error: "unknown database error" });
+  }
+});
+
+router.get("/rows/:count", async (req, res) => {
+  res.json(await addDataRows(req.params.count));
+});
 
 export default router;
--- database.js
+++ database.js
@@ -1,43 +1,56 @@
-import poolObj from './dbPool.js';
+import poolObj from "./dbPool.js";
 const { pool } = poolObj;
 
 let logonUsers = new Map();
 
 const sendQuery = async (sql, doCommit, ...params) => {
-    let conn, result
-    try {
-        conn = await pool.getConnection()
-        result = await conn.query(sql, params)
-        if (doCommit) {
-            await conn.query('COMMIT')
-        }
-    } catch (err) {
-		result = err
-        throw err
-    } finally {
-        if (conn)
-            conn.end()
-        return(result)
+  let conn, result;
+  try {
+    conn = await pool.getConnection();
+    result = await conn.query(sql, params);
+    if (doCommit) {
+      await conn.query("COMMIT");
     }
-}
+  } catch (err) {
+    result = err;
+    throw err;
+  } finally {
+    if (conn) conn.end();
+    return result;
+  }
+};
 
-const findOneUser = async (username) => sendQuery(`SELECT * FROM users WHERE username = ?`, true, username);
+const findOneUser = async (username) =>
+  sendQuery(`SELECT * FROM users WHERE username = ?`, true, username);
 
-const getAllData = async () => 
-    sendQuery(`SELECT * FROM data`);
+const getAllData = async () => sendQuery(`SELECT * FROM data`);
 
 const getDataById = async (id) =>
-    sendQuery(`SELECT * FROM data WHERE data.id = ?`, false, id);
+  sendQuery(`SELECT * FROM data WHERE data.id = ?`, false, id);
 
-const getAllUsers = async () => 
-    sendQuery(`SELECT * FROM users`);
+const getAllUsers = async () => sendQuery(`SELECT * FROM users`);
 
-const addOneUser = async (username, password) => 
-    sendQuery( `INSERT INTO users (username, password) VALUES (?, ?)`, false, username, password);
+const addOneUser = async (username, password) =>
+  sendQuery(
+    `INSERT INTO users (username, password) VALUES (?, ?)`,
+    false,
+    username,
+    password
+  );
 
-const addData = ({id, Firstname, Surname, userid}) =>
-    sendQuery(`INSERT INTO data (id, Firstname, Surname, userid) VALUES (?, ?, ?, ?)`, true, id, Firstname, Surname, userid);
+const addData = ({ id, Firstname, Surname, userid }) =>
+  sendQuery(
+    `INSERT INTO data (id, Firstname, Surname, userid) VALUES (?, ?, ?, ?)`,
+    true,
+    id,
+    Firstname,
+    Surname,
+    userid
+  );
 
+const addDataRows = async (rowCount) => {
+  const res = await sendQuery(`CALL addRows(?)`);
+};
 /*
 const getUserByName = (username) => 
     sendQuery(`SELECT * FROM users WHERE username = ?`, false, username);
@@ -46,13 +59,14 @@ const deleteData = (id, userid) =>
     sendQuery(`DELETE FROM data WHERE id = ? AND userid = ?`, true, id, userid);
 */
 export {
-    addOneUser,
-    getAllUsers,
-    findOneUser,
-    getAllData,
-    getDataById,
-    addData,
-    logonUsers,
-//    getUserByName,
-//    deleteData,
-}
+  addOneUser,
+  getAllUsers,
+  findOneUser,
+  getAllData,
+  getDataById,
+  addData,
+  addDataRows,
+  logonUsers,
+  //    getUserByName,
+  //    deleteData,
+};
